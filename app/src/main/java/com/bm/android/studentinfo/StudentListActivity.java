f3f547ad2fef013686707ca34ad5a247e3b8b312
bm71991 <ben.michael72591@gmail.com>
2019-04-05 13:03:15 -0400

    set up recyclerview and observer on livedata

--- app/src/main/java/com/bm/android/studentinfo/StudentListActivity.java
+++ app/src/main/java/com/bm/android/studentinfo/StudentListActivity.java
@@ -1,16 +1,46 @@
 package com.bm.android.studentinfo;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.bm.android.studentinfo.db.Student;
+
+import java.util.List;
 
 public class StudentListActivity extends AppCompatActivity {
+    private StudentListViewModel mStudentListViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_list);
+
+        /*configure student recyclerView (set adapter and layout manager)*/
+        RecyclerView recyclerView = findViewById(R.id.student_recyclerview);
+        final StudentListAdapter adapter = new StudentListAdapter(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        /*get ViewModel - ViewModel will persist over configuration changes*/
+        mStudentListViewModel = ViewModelProviders.of(this).get(StudentListViewModel.class);
+        /*set an observer on the ViewModel LiveData in order to alert the adapter
+        when the LiveData has changed.
+         */
+        mStudentListViewModel.getAllStudents().observe(this, new Observer<List<Student>>() {
+            @Override
+            public void onChanged(@Nullable List<Student> students) {
+                /*update student list in the adapter using DiffUtil*/
+                adapter.setStudents(students);
+            }
+        });
     }
 
     @Override
@@ -20,6 +50,13 @@ public class StudentListActivity extends AppCompatActivity {
     }
 
     //onOptionsItemSelected
-
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)    {
+        switch (item.getItemId())   {
+            case (R.id.add_student):
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
